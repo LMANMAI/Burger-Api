681e0bc6e55575074f0149ddb77b9b3652065953
Lucas Maidana <64815326+LMANMAI@users.noreply.github.com>
2024-03-24 17:59:17 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, Router } from "express";
 const cors = require("cors");
 import { DataBaseConnection } from "./config/db";
 import burgerRoute from "./routes/burger";
@@ -11,9 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 4000;
-// app.use("/", (req, res) => {
-//   res.send("Ruta de verificacion de funcionamiento, metodo GET funcionando");
-// });
+const healthCheckRouter: Router = express.Router();
+
+healthCheckRouter.get("/", (req: Request, res: Response) => {
+  res.send("Ruta de verificacion de funcionamiento, metodo GET funcionando");
+});
+
+app.use("/", healthCheckRouter);
 app.use("/burger", burgerRoute);
 
 app.listen(port, () => {
